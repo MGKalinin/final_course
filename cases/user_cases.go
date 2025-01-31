31105c383ef1455b909402e6a869c746b1775475
MG_Kalinin <1953115@gmail.com>
2025-01-31 23:20:17 +0300

    add change

--- cases/user_cases.go
+++ cases/user_cases.go
@@ -1,9 +1,7 @@
 package cases
 
 import (
-	"context"
 	"final_course/adapters"
-	"time"
 )
 
 // Service содержит логику работы сервиса
@@ -17,26 +15,4 @@ type Service struct {
 
 func NewService(storage adapters.Storage, client adapters.Client) *Service {
 	return &Service{storage: storage, client: client}
-}
-
-// UpdateRates обновляет курсы валют каждые 5 минут
-
-func (s *Service) UpdateRates(ctx context.Context) {
-	ticker := time.NewTicker(5 * time.Minute)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			titles := []string{"BTC", "ETH"}
-			coins, err := s.client.Get(ctx, titles)
-			if err != nil {
-				// Логирование ошибки (например, log.Println)
-				continue
-			}
-			s.storage.Store(ctx, coins)
-		case <-ctx.Done():
-			return
-		}
-	}
 }
