f00d96cba2a0f54d297d4c43f90cc2c2d6f0b288
MG_Kalinin <1953115@gmail.com>
2025-02-05 21:30:40 +0300

    add main

--- internal/main.go
+++ internal/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"final_course/internal/adapters"
+	"final_course/internal/cases"
 	"fmt"
 	"log"
 )
@@ -13,7 +14,7 @@ func main() {
 
 	// Инициализируем клиент с API ключом и базовым URL
 	apiKey := "1c5c331d210b7d08b2efe2c0741139b5063317d646e84f3619dd69a25d79f5a5"
-	client := adapters.NewClient(apiKey, "https://api.cryptocompare.com")
+	var client cases.Client = adapters.NewClient(apiKey, "https://api.cryptocompare.com")
 
 	// Делаем запрос для получения информации о криптовалюте BTH
 	coins, err := client.Get(ctx, []string{"BTH"})
