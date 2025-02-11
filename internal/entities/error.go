8c1381865b02482cbec6b318ea7f4179ed0d8317
MG_Kalinin <1953115@gmail.com>
2025-02-11 12:46:19 +0300

    add some minor

--- internal/entities/error.go
+++ internal/entities/error.go
@@ -5,6 +5,4 @@ import "github.com/pkg/errors"
 // создать переменную ошибки- и далее использовать её в обёртке во всех случаях,
 // добавляя соответсвующий комментарий
 
-// TODO: Переименовать var Error в понятное для всех название: ErrorEntity ??
-
 var ErrorInvalidParams = errors.New("invalid params:")
