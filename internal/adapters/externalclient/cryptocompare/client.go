810d4afcf07bbacc42140c4b465d3cc0a342ce77
MG_Kalinin <1953115@gmail.com>
2025-02-13 22:10:16 +0300

    added in order

--- internal/adapters/externalclient/cryptocompare/client.go
+++ internal/adapters/externalclient/cryptocompare/client.go
@@ -6,6 +6,7 @@ import (
 	"final_course/internal/entities"
 	"github.com/pkg/errors"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 )
@@ -27,40 +28,43 @@ func NewClient(httpClient *http.Client, url string) *Client { //TODO: конст
 
 // Get реализует метод интерфейса Client
 func (c *Client) Get(ctx context.Context, titles []string) ([]entities.Coin, error) { //TODO: здесь в слайс приходят монеты-нет монет-парсит по дефолту монеты
-	// базовый URL для запроса
-	url := c.baseURL + "/data/pricemulti"
 
-	// создаём HTTP GET запрос с контекстом-2
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	// Создаем объект url.Values
+	query := url.Values{}
+
+	//TODO: добавить проверку что нет монет-парсить по дефолту прописанные монеты(0-дефолт или
+	//		//TODO: не 0-то что прописано в слайс)-завести переменную-но нужно завести конфиг
+	// Устанавливаем query-параметры
+	if len(titles) > 0 {
+		query.Set("fsyms", strings.Join(titles, ","))
+	} else {
+		// Если titles пустой, используем монеты по умолчанию
+		defaultCoins := []string{"BTC", "ETH", "LTC"} // Пример монет по умолчанию
+		query.Set("fsyms", strings.Join(defaultCoins, ","))
+	}
+	query.Set("tsyms", "USD") // Предполагаем, что нам нужны курсы в долларах
+
+	// Cоздаём HTTP GET запрос с контекстом
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL+"/data/pricemulti", nil)
 	if err != nil {
 		return nil, errors.Wrap(entities.ErrorInvalidParams, "error creating request") //TODO: здесь ошибка парсинга
 	}
-
-	// Получаем объект url.Values
-	query := req.URL.Query()
-	//TODO:сначала параметры-потом реквест-потом ...3-получение ответа
-	// Устанавливаем query-параметры-1
-	if len(titles) > 0 { //TODO: добавить проверку что нет монет-парсить по дефолту прописанные монеты(0-дефолт или не 0-то что прописано в слайс)-завести переменную-но нужно завести конфиг
-		query.Set("fsyms", strings.Join(titles, ","))
-		query.Set("tsyms", "USD") // Предполагаем, что нам нужны курсы в долларах
-	}
-
 	// Обновляем URL запроса
 	req.URL.RawQuery = query.Encode()
 
-	// выполняем запрос-3
+	// Выполняем запрос
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, errors.Wrap(entities.ErrorInvalidParams, "failed to execute request") //TODO: тут другая ошибка-она не внутренняя уже
 	}
 	defer resp.Body.Close()
 
-	// проверяем статус ответа
+	// Проверяем статус ответа
 	if resp.StatusCode != http.StatusOK {
 		return nil, errors.Wrap(entities.ErrorInvalidParams, "failed to get data") //TODO: тут другая ошибка-она не внутренняя уже
 	}
 
-	// распарсить JSON ответ в слайс структур Coin
+	// Распарсить JSON ответ в слайс структур Coin
 	var data map[string]map[string]float64
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
 		return nil, errors.Wrap(entities.ErrorInvalidParams, "failed to decode response") //TODO: тут другая ошибка-она не внутренняя уже-errorf
