2f6398f12931d7f97f40d5080389a1901fa5c067
MG_Kalinin <1953115@gmail.com>
2025-03-13 22:18:06 +0300

    don`t work ....

--- internal/port/apiserver/routes.go
+++ internal/port/apiserver/routes.go
@@ -1,8 +1,11 @@
 package apiserver
 
 import (
+	"context"
+	"final_course/internal/cases"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
 )
 
 // SetupRoutes регистрирует маршруты API
@@ -19,4 +22,34 @@ func (h *RESTHandler) SetupRoutes(r chi.Router) {
 		r.Get("/coins/min", h.getMinRate)
 		r.Get("/coins/avg", h.getAvgRate)
 	})
+}
+
+func (h *RESTHandler) getMaxRate(w http.ResponseWriter, r *http.Request) {
+	titles := []string{"title1", "title2"} // Замените на реальные данные
+	coins, err := h.service.GetMaxRate(context.Background(), titles)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Вернуть данные пользователю
+}
+
+func (h *RESTHandler) getMinRate(w http.ResponseWriter, r *http.Request) {
+	titles := []string{"title1", "title2"} // Замените на реальные данные
+	coins, err := h.service.GetMinRate(context.Background(), titles)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Вернуть данные пользователю
+}
+
+func (h *RESTHandler) getAvgRate(w http.ResponseWriter, r *http.Request) {
+	titles := []string{"title1", "title2"} // Замените на реальные данные
+	coins, err := h.service.GetAvgRate(context.Background(), titles)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Вернуть данные пользователю
 }
